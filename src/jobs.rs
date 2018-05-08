d04c421cd8221e5d52242890f93880c04b778147
geppetto <wkim149@gmail.com>
2018-05-08 16:07:28 -0400

    add absolute time

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,12 +1,13 @@
 use std::fs;
 use std::fs::File;
+use std::process::exit;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::OpenOptions;
 
 use serde_json;
 use rand::{Rng, thread_rng};
-use chrono::{Duration, Local};
+use chrono::{Duration, Local, Timelike, Datelike};
 use chrono::naive::NaiveDateTime;
 
 pub const JOB_FILE: &'static str = concat!(env!("HOME"), "/.local/share/scoop/jobs");
@@ -24,6 +25,11 @@ pub struct Job {
 
 pub fn add_job(time: &str, cmd: &str, args: &Vec<String>) {
         let t = parse_time(time).expect("Invalid time format.");
+        if t < Local::now().naive_local() {
+                eprintln!("Specified time is in the past.");
+                exit(1);
+        }
+
         let job = Job { 
                 id: format!("{:04x}", thread_rng().gen::<u16>()),
                 time: t.format(TIME_FORMAT).to_string(), 
@@ -41,18 +47,15 @@ pub fn add_job(time: &str, cmd: &str, args: &Vec<String>) {
         let fr = File::open(JOB_FILE).unwrap();
         for line in BufReader::new(fr).lines() {
                 let line = line.unwrap();
-                if inserted {
-                        writeln!(&mut fw, "{}", line).unwrap();
-                        continue;
-                }
-
-                let line_job: Job = serde_json::from_str(&line).unwrap();
-                let line_t = NaiveDateTime::parse_from_str(&line_job.time, TIME_FORMAT).unwrap();
-                /* insert if new job's time is earlier than line's */
-                if t < line_t {
-                        serde_json::to_writer(&fw, &job).unwrap();
-                        write!(&mut fw, "\n").unwrap();
-                        inserted = true;
+                if !inserted {
+                        let ljob: Job = serde_json::from_str(&line).unwrap();
+                        let ltm = NaiveDateTime::parse_from_str(&ljob.time, TIME_FORMAT).unwrap();
+                        /* insert if new job's time is earlier than line's */
+                        if t < ltm {
+                                serde_json::to_writer(&fw, &job).unwrap();
+                                write!(&mut fw, "\n").unwrap();
+                                inserted = true;
+                        }
                 }
                 /* insert the line back */
                 writeln!(&mut fw, "{}", line).unwrap();
@@ -72,23 +75,55 @@ fn parse_time(s: &str) -> Option<NaiveDateTime> {
         let now = Local::now().naive_local();
         if s.starts_with("+") {
                 let (_, t) = s.split_at(1);
-                t.split('.')
-                        .map(parse_duration)
-                        .fold(Some(now), |acc, x| acc.and_then(|y| y.checked_add_signed(x)))
+                t.split('.').fold(Some(now), parse_duration)
+        } else if s.starts_with("@") {
+                let (_, t) = s.split_at(1);
+                t.split('.').fold(Some(now), parse_instant)
         } else {
                 None
         }
 }
 
-fn parse_duration(t: &str) -> Duration {
+fn parse_instant(now: Option<NaiveDateTime>, t: &str) -> Option<NaiveDateTime> {
+        if now.is_none() {
+                return None;
+        }
+
+        let now = now.unwrap();
         let (n, c) = t.split_at(t.len() - 1);
-        let n = n.parse().expect("Invalid time format.");
-        match c {
-                "s" => Duration::seconds(n),
-                "m" => Duration::minutes(n),
-                "h" => Duration::hours(n),
-                "d" => Duration::days(n),
-                _ => panic!("Invalid time format.")
+        if let Ok(n) = n.parse() {
+                match c {
+                        "s" => now.with_second(n),
+                        "m" => now.with_minute(n),
+                        "h" => now.with_hour(n),
+                        "D" => now.with_day(n),
+                        "M" => now.with_month(n),
+                        "Y" => now.with_year(n as i32),
+                        _ => return None
+                }
+        } else {
+                None
+        }
+}
+
+fn parse_duration(now: Option<NaiveDateTime>, t: &str) -> Option<NaiveDateTime> {
+        if now.is_none() {
+                return None;
+        }
+
+        let now = now.unwrap();
+        let (n, c) = t.split_at(t.len() - 1);
+        if let Ok(n) = n.parse() {
+                let a = match c {
+                        "s" => Duration::seconds(n),
+                        "m" => Duration::minutes(n),
+                        "h" => Duration::hours(n),
+                        "d" => Duration::days(n),
+                        _ => return None
+                };
+                now.checked_add_signed(a)
+        } else {
+                None
         }
 }
 
@@ -118,8 +153,7 @@ pub fn list_jobs() {
         
         println!("{:4}\t{:19}\t{}", "ID", "TIME", "COMMAND");
         for line in BufReader::new(f).lines() {
-                let line = line.unwrap();
-                let job: Job = serde_json::from_str(&line).unwrap();
+                let job: Job = serde_json::from_str(&line.unwrap()).unwrap();
                 println!("{}\t{}\t{}", job.id, job.time, job.cmd);
         };
 }
